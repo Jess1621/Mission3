ea145c25779f8502684565a57ebeae1e9b14a21c
Jess1621 <jesshansen70@gmail.com>
2022-01-19 18:49:59 -0700

    Mission 3 Files

---
+++ Mission2/Models/GradeCalculatorModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+//To be able to use data validation
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class GradeCalculatorModel
+    {
+        //Makes the field required to fill out and makes it so the input has to be a value between 0 and 100
+        [Required, Range(0,100)]
+        public int Assignments { get; set; }
+
+        [Required, Range(0,100)]
+        public int Project { get; set; }
+
+        [Required, Range(0, 100)]
+        public int Quizzes { get; set; }
+
+        [Required, Range(0, 100)]
+        public int Exams { get; set; }
+
+        [Required, Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
---
+++ Mission2/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,38 @@
+﻿// JQuery Form Function
+$("#btnSend").click(function () {
+    let fGradeAssignments = $("#iAssignments").val();
+    let fGradeGroupProject = $("#iGroupProject").val();
+    let fGradeQuizzes = $("#iQuizzes").val();
+    let fGradeExams = $("#iExams").val();
+    let fGradeIntex = $("#iIntex").val();
+
+    let iGrades = parseFloat(fGradeAssignments) + parseFloat(fGradeGroupProject) +
+        parseFloat(fGradeQuizzes) + parseFloat(fGradeExams) + parseFloat(fGradeIntex)
+
+    // Loop to determine grade value
+    if (iGrades >= 100 && iGrades >= 94) {
+        sGradeLetter = "A"
+    } else if (iGrades < 94 && iGrades >= 90) {
+        sGradeLetter = "A-"
+    } else if (iGrades < 90 && iGrades >= 87) {
+        sGradeLetter = "B+"
+    } else if (iGrades < 87 && iGrades >= 84) {
+        sGradeLetter = "B"
+    } else if (iGrades < 84 && iGrades >= 80) {
+        sGradeLetter = "B-"
+    } else if (iGrades < 80 && iGrades >= 77) {
+        sGradeLetter = "C+"
+    } else if (iGrades < 77 && iGrades >= 74) {
+        sGradeLetter = "C"
+    } else if (iGrades < 74 && iGrades >= 70) {
+        sGradeLetter = "C-"
+    } else if (iGrades < 70 && iGrades >= 67) {
+        sGradeLetter = "D+"
+    } else if (iGrades < 67 && iGrades >= 64) {
+        sGradeLetter = "D"
+    } else if (iGrades < 64 && iGrades >= 60) {
+        sGradeLetter = "D-"
+    } else sGradeLetter = "E"
+
+    document.getElementById("formResults").innerHTML = "You got a(n) " + iGrades + "% which is a(n) " + sGradeLetter + "!";
+})
